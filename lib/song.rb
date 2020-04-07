0b9d0351bb16f07c9def4cafb12a1a630bd96a07
rpdecks <rpdecks@gmail.com>
2020-04-07 12:18:33 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -13,9 +13,9 @@ class Song
     super
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@songs.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@songs
